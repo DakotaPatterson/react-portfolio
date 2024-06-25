bae085a9d9b9731de32b493fda0cea6e4a5ecd66
DakotaPatterson <158085432+DakotaPatterson@users.noreply.github.com>
2024-06-25 14:06:39 -0400

    Photos are added and styled

--- react-portfolio/src/components/projectData.jsx
+++ react-portfolio/src/components/projectData.jsx
@@ -1,43 +1,50 @@
+import codingQuizImg from '../assets/coding-quiz.png';
+import weatherDashboardImg from '../assets/weather-dashboard.png';
+import gallowScreenImg from '../assets/gallow-screen.png';
+import workDayImg from '../assets/workDay.png';
+import passwordImg from '../assets/password.png';
+import noteTakerImg from '../assets/note-taker.png';
+
 const projectsData = [
     {
       title: 'Coding Quiz',
       description: 'A coding quiz to help familiarize students with a coding assessment.',
-      image: '../assets/coding-quiz.png',
+      image: codingQuizImg,
       deployedUrl: 'https://dakotapatterson.github.io/Coding-Quiz/',
       githubUrl: 'https://github.com/DakotaPatterson/Coding-Quiz'
     },
     {
       title: 'Weather Dashboard',
       description: 'A weather dashboard with the 5 day forecast.',
-      image: '../assets/weather-dashboard.png',
+      image: weatherDashboardImg,
       deployedUrl: 'https://dakotapatterson.github.io/weather-dashboard/',
       githubUrl: 'https://github.com/DakotaPatterson/weather-dashboard'
     },
     {
         title: 'Guesser Gallow',
         description: 'Guesser Gallows is a lightweight, browser based game of hangman.',
-        image: '../assets/gallow-screen.png',
+        image: gallowScreenImg,
         deployedUrl: 'https://guesser-gallows-04ed44eee1bd.herokuapp.com/',
         githubUrl: 'https://github.com/Shrcker/Guesser-Gallow'
       },
       {
         title: 'Work Day Scheduler',
         description: 'A work day scheduler to help the user keep track of important events happening that day.',
-        image: '../assets/workDay.png',
+        image: workDayImg,
         deployedUrl: 'https://dakotapatterson.github.io/work-day-scheduler/',
         githubUrl: 'https://github.com/DakotaPatterson/work-day-scheduler'
       },
       {
         title: 'Password Generator',
         description: 'A generator for secure passwords.',
-        image: '../assets/password.png',
+        image: passwordImg,
         deployedUrl: 'https://dakotapatterson.github.io/Password-generator/',
         githubUrl: 'https://github.com/DakotaPatterson/Password-generator'
       },
       {
         title: 'Note Taker',
         description: 'A Note Taker that uses the user input to generate a list of notes using express.js.',
-        image: '../assets/note-taker.png',
+        image: noteTakerImg,
         deployedUrl: 'https://note-taker101-1c17bd601aa1.herokuapp.com/',
         githubUrl: 'https://github.com/DakotaPatterson/NoteTaker?tab=readme-ov-file'
       },
--- react-portfolio/src/pages/Portfolio.jsx
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import projectsData from '../components/projectData'; 
+import '../App.css';
 
 
 export default function Portfolio() {
@@ -10,18 +11,24 @@ export default function Portfolio() {
       <Row xs={1} md={2} lg={3} className="g-4">
         {projectsData.map((project, index) => (
           <Col key={index}>
-            <Card>
+            <Card className="h-100">
+            <div className="image-container">
               <Card.Img variant="top" src={project.image} />
-              <Card.Body>
+              </div>
+              <Card.Body className="d-flex flex-column">
                 <Card.Title>{project.title}</Card.Title>
                 <Card.Text>{project.description}</Card.Text>
+                <div className="mt-auto">
                 <Button variant="primary" href={project.deployedUrl} target="_blank" rel="noopener noreferrer">View Application</Button>{' '}
                 <Button variant="secondary" href={project.githubUrl} target="_blank" rel="noopener noreferrer">View GitHub</Button>
+                </div>
               </Card.Body>
             </Card>
           </Col>
         ))}
       </Row>
+      <br></br>
+      <br></br>
     </Container>
   );
 }
